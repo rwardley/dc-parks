24f6e32e8f22de31c07e84f3cadacd45f0f053a4
Rosemary Wardley <rosemary.wardley@gmail.com>
2018-07-19 13:19:57 -0400

    changed api key

--- php/cartoDBProxy.php
+++ php/cartoDBProxy.php
@@ -5,7 +5,8 @@ function goProxy($dataURL)
 {
 	$baseURL = 'http://rwardley.cartodb.com/api/v2/sql?';
 	//  					^ CHANGE THE 'CARTODB-USER-NAME' to your cartoDB url!
-	$api = '&api_key=430e489da8b384a29c4121ad4e4c9f3a0c758079';
+	//$api = '&api_key=430e489da8b384a29c4121ad4e4c9f3a0c758079';
+    $api = '&api_key=NbKRhfqoRNEQO9AdGilNaA';
 	//				 ^ENTER YOUR API KEY HERE!
 	$url = $baseURL.'q='.urlencode($dataURL).$api;
 	$result = file_get_contents ($url);
